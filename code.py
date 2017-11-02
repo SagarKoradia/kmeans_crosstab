24a216eddcbc14b5aa2682c7340f0aeb69e2cb26
Sagar Koradia <31327738+SagarKoradia@users.noreply.github.com>
2017-11-03 01:28:58 +1100

    Create code.py

---
+++ code.py
@@ -0,0 +1,16 @@
+import numpy
+import pandas as pd
+from sklearn.cluster import KMeans
+
+fn = r'C:\Users\DELL I5558\Desktop\Python\ELEC5222\kMeans\NSW-ER01.csv'
+dataframe = pd.read_csv(fn)
+dataset = dataframe.values
+X = dataset[:, 0:23].astype(float)
+seasons = dataset[:, 23]
+
+model = KMeans(n_clusters=4)
+labels = model.fit_predict(X)
+
+dff = pd.DataFrame({'labels': labels, 'seasons': seasons})
+ct = pd.crosstab(dff['labels'], dff['seasons'])
+print(ct)
